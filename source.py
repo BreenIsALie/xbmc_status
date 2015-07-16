941d0d8f58053c05b02237c4d45f52afd28efc8a
BreenIsALie <m.jonsson2@gmail.com>
2015-07-16 20:44:15 +0200

    * Removed source.py (non functional) * Renamed source2 to source.py, new main function

--- source.py
+++ source.py
@@ -1,53 +1,69 @@
+__author__ = 'BreenIsALie'
 # Script for reading status of local linux hosts
 
 ## NOTES ##
+# The "Ask to exit" functionality has been disabled, easier to just re-run the command. Might re-enable. Kept for now
+# Functionality:    Hostname
+#                   HDD Usage
+#                   IP addresses
+#                   System version
+#                   Current system time
+#                   Memory statistics
 
 
+import subprocess   # Needed to run linux commands using subprocess.call
+import socket       # Used to get the hostname
+from sys import argv    # Used to take arguments from the command line when starting the script (currently just name)
+import time             # Used for the time.sleep() function
+import platform
 
-import socket  # Used to read system info from host
-import os
-import subprocess
-import commands
+script_name = argv  # saves the script name as a variable, used in the message at the end. Not *strictly* neccessary
 
-# CONFIG SECTION START
+def get_hostname():  # Gets the hostname and returns it to the main program
+    host = socket.gethostname()
+    return host
 
-hdd1 = "/"  # Path to check disk usage for, one for each disk. Add relevant paths here
-hdd2 = "/HDD2/"  # Path to check disk usage for, one for each disk. Add relevant paths here
-#  hdd* = "[path]"
+def get_main_disk_usage():  # Runs the linux command df -h to get an overview over hdd usage
+        p1 = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE)  # runs the df -h command
+        p2 = subprocess.Popen(('grep', 'sd..'), stdin=p1.stdout)  # runs grep on the df output to trim out non HDDs
+        return p2  # returns the trimmed down output, now containing only the HDDs
 
-# CONFIG SECTION END
+def get_ip_addresses():
+    p3 = subprocess.Popen((["ifconfig"]), stdout=subprocess.PIPE)
+    p4 = subprocess.Popen((["grep", "inet addr"]), stdin=p3.stdout)
+    print "\nIP ADDRESSES:"
 
-def user_prompt():  # Asks the user if to shutdown or refresh. Also used to keep info on screen
-    print("Press enter to refresh the program, or type exit to end the program")
-    user_input = raw_input(">> ")
+
+def user_interaction(name):  # Used as"what now" question at the end. Allows refresh (press enter) or exit (type exit)
+    print "\n\nThis is the %s script. Type exit to end the script, or press enter to run it again" % name
+    user_input = raw_input("> ")
     return user_input
 
-def disk_free():
-    # free_space = commands.getstatusoutput("df -k | grep 'sd..'")
-    process = subprocess.Popen(["df -k | grep 'sd.."], shell=False, stdout=subprocess.PIPE)
-    data = process.communicate()
-    free_space = int(data[0].spilit()[5][1:3])
-    return free_space
+def get_mem_usage():
+    vmstat = subprocess.Popen((["vmstat", "-s", "-S", "M"]),stdout=subprocess.PIPE)
+    head = subprocess.Popen((["head"]), stdin=vmstat.stdout)
 
-def get_ip():
-    ip = commands.getstatusoutput("ifconfig | grep 'inet addr:'")
-    return ip
+if __name__ == '__main__':  # Main function goes here
+        hostname = get_hostname()
 
-exit_sequence = 0  # Defines the exit sequence to 0 so the loop can run
-while exit_sequence != "exit":  # Run the loop until the user says to exit using user_prompt():
+        print "\n\n-----------------------------------------------------------------------------\n\n"  # For readability
+        print "HOSTNAME: %s" "\n" % hostname    # Prints the hostname
 
-    hostname = socket.gethostname()  # Gets the hostname of the system
-    ip_address = get_ip()  # Gets the IP of the host
-    disk_space = disk_free()  # Uses the hdd1 variable (path to disk 1) to get the amount of used storage
+        sys_info = platform.platform()
+        print "SYSTEM: %s" % sys_info
 
-    # For debug use
-    print ("%s") % hostname
-    print ("%s") % ip_address
-    print ("%s") % disk_space
-    exit_sequence = user_prompt()
+        sys_time = time.asctime()
+        print "\nSYSTEM TIME: %s" % sys_time
 
-exit()
+        get_mem_usage()
+        print"\nMEMORY USAGE: \n"
+        time.sleep(0.1)
 
+        print "\nDISK USAGE: "
+        get_main_disk_usage()                   # Prints the output of df-h. Maybe trim a bit in later versions
 
+        get_ip_addresses()
 
+        time.sleep(0.1)  # Holds script for 0.1 second, stopping the script from outpacing linux commands
+        print "\n\n-----------------------------------------------------------------------------\n\n"  # For readability
 
