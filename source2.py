2215fa2f3125af04fad6ee8a13b4c3a1b3f520b1
BreenIsALie <m.jonsson2@gmail.com>
2015-07-16 17:20:16 +0200

    Fixed the ability to check the hdd use. Functional with hostname and hdd use discovery. More function to be added

--- source2.py
+++ source2.py
@@ -2,6 +2,8 @@ __author__ = 'BreenIsALie'
 # Script for reading status of local linux hosts
 
 ## NOTES ##
+# Current functionality:    Gets Hostname
+#                           Gets HDD Usage
 
 import commands
 import subprocess
@@ -11,18 +13,12 @@ from sys import argv
 
 script_name = argv
 
-def user_prompt():  # Asks the user if to shutdown or refresh. Also used to keep info on screen
-    print("Press enter to refresh the program, or type exit to end the program")
-    user_input = raw_input(">> ")
-    return user_input
-
 def get_hostname():
     hostname = socket.gethostname()
     return hostname
 
 def get_main_disk_usage():
-    disk_use = subprocess.Popen(["df", "-h"], shell=False, stdout=subprocess.PIPE)
-    return disk_use
+        subprocess.call(["df", "-h"])
 
 def user_interaction(name):
     print "This is the %s script. Type exit to end the script, or press enter to run it again" % name
@@ -33,8 +29,8 @@ if __name__ == '__main__':
     exit_condition = 0
     while exit_condition != "exit":
         hostname = get_hostname()
-        hdd_usage = get_main_disk_usage()
 
-        print "HOSTNAME: %s" % hostname
-        print "HDD Usage: %s" % hdd_usage
+        print "HOSTNAME: %s" "\n" % hostname
+        print "DISK USAGE: \n"
+        get_main_disk_usage()
         exit_condition = user_interaction(script_name)
